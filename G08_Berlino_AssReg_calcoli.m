22f964af8a00be2d0a5f30c2e12d44b85c4f516f
Jfkmdd <federico.deduro@gmail.com>
2021-12-20 17:40:16 +0100

    Modifica grafici
    Modificati i grafici per concentrazioni, eventi climatici e per carburanti e aggiunta una sezione di statistiche utili seguite a lezione (Gio 16 Dicembre)

--- G08_Berlino_AssReg_calcoli.m
+++ G08_Berlino_AssReg_calcoli.m
@@ -4,25 +4,26 @@
 % Ciullo Roberto        1074568
 % Bouchemal Saif        1074800
 
-table = load("G08.mat")
-table.t
+% Grafico per concentrazioni
+stackedplot(t, {'NO2', 'NOx', 'PM10', 'O3'}, "XVariable","Data")
+matrice_di_correlazione_gas = corrcoef(t{:, {'NO2', 'NOx', 'PM10', 'O3'}})
 
-% Grafico per concentrazioni NO2
-datetime('')
-x = datetime('today') + caldays(1:length(table.t.NO2));
-plot(x,table.t.NO2, "LineStyle","none", "Marker","hexagram")
-title('Concentrazioni di NO2')
-xlabel('Mese')
-ylabel('Concentrazione mug / m^3')
+% Grafico per eventi climatici
+stackedplot(t, {'Temperatura', 'Umidita_relativa', 'Pioggia_cum'}, "XVariable","Data")
+matrice_di_correlazione_clima = corrcoef(t{:, {'Temperatura', 'Umidita_relativa', 'Pioggia_cum'}})
 
-% Grafico per concentrazioni NOx
-title('Concentrazioni di NOx')
-plot(table.t.NO2, "LineStyle","none", "Marker","hexagram")
+% Grafico per carburanti
+stackedplot(t, {'Benzina_vendita_rete_ord', 'Gasolio_motori_rete_ord', 'Gasolio_riscaldamento'}, "XVariable","Data")
+matrice_di_correlazione_carburanti = corrcoef(t{:, {'Benzina_vendita_rete_ord', 'Gasolio_motori_rete_ord', 'Gasolio_riscaldamento'}})
 
-% Grafico per concentrazioni PM10
-title('Concentrazioni di PM10')
-plot(table.t.PM10, "LineStyle","none", "Marker","hexagram")
+% Stats
+stackedplot(t, {'NO2', 'Benzina_vendita_rete_ord'}, "XVariable","Data")
+scatter(t.Benzina_vendita_rete_ord, t.NO2)
+lsline
 
-% Grafico per concentrazioni O3
-title('Concentrazioni di O3')
-plot(table.t.O3, "LineStyle","none", "Marker","hexagram")
+% Dipendenze con NO2
+vn = t.Properties.VariableNames'
+fitlm(t(:, [vn(end-8:end); vn(end-9)]))
+corrcoef(t{:, vn(end-8:end)})
+
+
